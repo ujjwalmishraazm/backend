bf34c451a801da02d0c9c480051a4105551b9e3a
ujjwalmishraazm <ujjwalmishra9598@gmail.com>
2026-01-03 16:01:51 +0530

    commit

--- src/server.js
+++ src/server.js
@@ -2,21 +2,21 @@
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./confiq/db.js";
-
+import taskRoutes from "./routes/taskRoute.js"
 dotenv.config();
 
 const app = express();
 
 
 app.use(cors({
-  origin: 'https://frontend-iota-five-27.vercel.app/', // your frontend URL
+  origin: 'https://frontend-iota-five-27.vercel.app', // your frontend URL
   credentials: true,
 }));
 
 app.use(express.json());
 
- await connectDB()
-
+ connectDB()
+app.use("/api/tasks", taskRoutes);
 app.get("/", (req, res) => {
   res.send("API running");
 });
