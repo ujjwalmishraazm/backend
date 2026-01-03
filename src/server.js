d220e65cd25705f02b398ffcd87521560ec3cc8a
ujjwalmishraazm <ujjwalmishra9598@gmail.com>
2026-01-03 14:26:00 +0530

    ..

--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import connectDB from './confiq/db.js'
-import router from './routes/taskRoute.js'
+  import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
-connectDB()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/tasks', router)
+ await connectDB()
 
-app.get('/', (_, res) => {
-  res.send('API Running')
-})
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
-)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
